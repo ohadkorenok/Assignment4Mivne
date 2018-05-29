8d9198c994349d236dbbac628935e61a54dbf3f9
Ohad <ohadkorenok@gmail.com>
2018-05-29 11:38:19 +0300

    finished. TODO:: documentation, and abit of reviews.

--- Runner.java
+++ Runner.java
@@ -11,43 +11,40 @@ public class Runner {
 	//2. m (the size of expected hash table with chaining).
 	//Note that the files used (friends.txt,messages.txt,spam_words.txt) 
 	//should be located in the same location as your src folder.
-	
+
 	public static void main(String[] args) {
-		
+
 		//Create the btree using the t value and the path to the friends file.
 		BTree btree = createTree(args[0]);
 
 		//Get the BFS representation of the btree
-//		String treeLayout = btree.toString();
-//		System.out.println(treeLayout);
+		String treeLayout = btree.toString();
 
-		boolean bfsscan = btree.BFSScan();
-		System.out.println("ok!");
-//		// Create a Messages object based on the messages file.
-//		Messages messages = createArrayOfMessages();
-//
-//		//For each message, create a hash table based on the words in the message.
-//		messages.createHashTables(args[1]);
+		// Create a Messages object based on the messages file.
+		Messages messages = createArrayOfMessages();
+
+		//For each message, create a hash table based on the words in the message.
+		messages.createHashTables(args[1]);
 
 		//Find the spam messages- Use the path of the spam_words file.
 		//	(The Messages object should use the file path to create the Spam array.
 		//	Then, it will determine for each message if it is spam, using the Spam array,
 		//	the btree and the hash table of the message).
-//		String spamMessages = messages.findSpams(System.getProperty("user.dir")+"/spam_words.txt", btree);
+		String spamMessages = messages.findSpams(System.getProperty("user.dir")+"/spam_words.txt", btree);
 
 		//create a file with the program's expected output
-//		createOutputFile(treeLayout, spamMessages, System.getProperty("user.dir")+"/output.txt");
+		createOutputFile(treeLayout, spamMessages, System.getProperty("user.dir")+"/output.txt");
 
 	}
 
 
 	// Create a Messages object based on the messages file.
 	// Creates a Messages object and updates its' Message array using the messages file.
-//	private static Messages createArrayOfMessages() {
-//		Messages messages = new Messages();
-//		messages.generateMessages(System.getProperty("user.dir")+"/messages.txt");
-//		return messages;
-//	}
+	private static Messages createArrayOfMessages() {
+		Messages messages = new Messages();
+		messages.generateMessages(System.getProperty("user.dir")+"/messages.txt");
+		return messages;
+	}
 
 
 
@@ -55,8 +52,7 @@ public class Runner {
 	// Inserts the friends into the tree.
 	private static BTree createTree(String tVal) {
 		BTree btree = new BTree(tVal);
-		System.out.println(tVal);
-		btree.createFullTree(System.getProperty("user.dir")+ "/friends.txt");
+		btree.createFullTree(System.getProperty("user.dir")+"/friends.txt");
 		return btree;
 	}
 
@@ -66,8 +62,8 @@ public class Runner {
 		Path path = Paths.get(pathToOutput);
 		StringBuilder sb = new StringBuilder();
 		sb.append(treeLayout).append(System.lineSeparator()).append(spamMessages);
-	    byte[] strToBytes = sb.toString().getBytes();
-	    try {
+		byte[] strToBytes = sb.toString().getBytes();
+		try {
 			Files.write(path, strToBytes);
 		} catch (IOException e) {
 			e.printStackTrace();
